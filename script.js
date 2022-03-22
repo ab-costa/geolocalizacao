de6ce3035b8d657e51c6b0e9948b7cb0034e3433
ab-costa <costa.alessandroborges@gmail.com>
2022-03-22 01:50:58 -0300

    Alterações html/css

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let latitude = document.getElementById("latitude")
 let longitude = document.getElementById("longitude")
 let iniciarTrekking = document.getElementById("iniciarTrekking")
 let finalizarTrekking = document.getElementById("finalizarTrekking")
+let cardLatitudeLongitude = document.getElementById("card-latitude-longitude")
+let bodydBox = document.getElementById("body_box")
 
 let encerrarTrekking = false
 
@@ -17,7 +19,7 @@ button.addEventListener("click", () => {
 })
 
 iniciarTrekking.addEventListener("click", () => {
-    encerrarTrekking = false
+    // encerrarTrekking = false
 
     function getGeolocalization() {
         if(!encerrarTrekking) {
@@ -25,10 +27,17 @@ iniciarTrekking.addEventListener("click", () => {
                 let lat = position.coords.latitude
                 let long = position.coords.longitude
     
+                preencherTrekking(lat, long)
+
                 console.log(`LATITUDE: ${lat}`)
                 console.log(`LONGITUDE: ${long}`)
             })
         } else {
+            let fimTrekking = document.createElement('div');
+            fimTrekking.classList.add("fim_trekking");
+            fimTrekking.innerText = "TREKKING ENCERRADO"
+            cardLatitudeLongitude.appendChild(fimTrekking)
+
             console.log("Trekking encerrado")
             clearInterval(intervalId)
         }
@@ -40,3 +49,20 @@ iniciarTrekking.addEventListener("click", () => {
 finalizarTrekking.addEventListener("click", () => {
     encerrarTrekking = true
 })
+
+function preencherTrekking(latitude, longitude) {
+    let div = document.createElement('div');
+    div.classList.add("box_latitude_longitude");
+
+    let spanLatitude = document.createElement('span');
+    spanLatitude.classList.add("latitude_longitude")
+    spanLatitude.innerText = `LATITUDE: ${latitude}`
+
+    let spanLongitude = document.createElement('span');
+    spanLongitude.classList.add("latitude_longitude")
+    spanLongitude.innerText =`LONGITUDE: ${longitude}`
+
+    div.appendChild(spanLongitude)
+    div.appendChild(spanLatitude)
+    cardLatitudeLongitude.appendChild(div)
+}
